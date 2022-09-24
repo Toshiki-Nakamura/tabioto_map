3886ed3bf1031981e3b0f37280030bce6a368fba
shake551 <hiroki.yamako@ezweb.ne.jp>
2022-09-25 04:05:33 +0900

    fix user response

--- lib/repository/user.dart
+++ lib/repository/user.dart
@@ -14,18 +14,26 @@ class User {
   static Future<User?> login(String userName) async {
     try {
       print("request start");
-      final result = await post(Uri.http('0.0.0.0:8000', 'user'),
-          body: {"name": userName}
+
+      final body = jsonEncode({
+        'name': userName,
+      });
+
+      final result = await post(
+        Uri.http('0.0.0.0:8000', 'user'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body,
       );
 
       Map<String, dynamic> data = jsonDecode(result.body);
 
       User user = User(
         id: data['id'],
-        // TODO: レスポンスから表示する
-        name: userName,
+        name: data['name'],
       );
-
+      
       return user;
     } catch (e) {
       return null;
